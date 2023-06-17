ce4f8c406318e0a9c658a848e5a717b09fec9926
Oleksandr Petryk <petryk.olexandr@gmail.com>
2023-06-17 12:01:10 +0300

    df

--- TestApp/Class1.cs
+++ TestApp/Class1.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace TestApp
 {
     internal class Class1
     {
+        Console.WriteLine("Class 1");
+
     }
 }
